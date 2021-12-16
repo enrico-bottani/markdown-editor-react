98a40b135ce8af02002646407c9568a44399dd4d
Enrico <enrico@cryms.com>
2021-12-16 23:00:21 +0100

    Layout changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
+import ReactMarkdown from 'react-markdown'
+import CodeMirror from "@uiw/react-codemirror";
+import "codemirror/theme/monokai.css";
 import './App.css';
+import 'bootstrap';
+
+const code = 'const a = 0;';
+
 
 function App() {
+  let code = "# Hello, **world**!";
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        <div className='container'>
+          <div className="row">
+            <div className="col-6">
+              <CodeMirror value={code} height={"100%"} options={{
+                theme: "monokai",
+                mode: "markdown"
+              }}
+              />
+            </div>
+            <div className="col-6">
+              <ReactMarkdown>{code}</ReactMarkdown>
+            </div>
+          </div>
+        </div>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
