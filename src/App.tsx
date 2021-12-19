89d628237e6243d3ea3e252caa2b5cd532c9a621
Enrico <enrico@cryms.com>
2021-12-19 19:22:52 +0100

    You can write and mk will render now...

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactMarkdown from 'react-markdown'
-import CodeMirror from "@uiw/react-codemirror";
+import CodeMirror, {Editor} from "@uiw/react-codemirror";
 import "codemirror/theme/monokai.css";
 import './App.css';
 import 'bootstrap';
 
 const code = 'const a = 0;';
 
+type ExcerciseProps = {
+  title: string
+}
 
 function App() {
-  let code = "# Hello, **world**!";
+  const [code, setCode] = useState<string>("# Hello, **world**!");
+
+  let callback = function(editor: Editor,data:any){
+    console.log(editor.getValue())
+    setCode(editor.getValue())
+  }
+
   return (
+    
     <div className="App">
       <header className="App-header">
         <div className='container'>
-          <div className="row">
+          <div className="row editor-row">
             <div className="col-6">
-              <CodeMirror value={code} height={"100%"} options={{
+              <CodeMirror value={code} onChange={callback} height={300} options={{
                 theme: "monokai",
                 mode: "markdown"
               }}
